590243433762922f46de871e5686fa43c52cbf66
udacity-tom <udacity@iamtom.com>
2020-12-11 13:50:32 +0100

    Basic webpack/Sass/entry/html added

---
+++ src/client/index.js
@@ -0,0 +1,36 @@
+import 'regenerator-runtime/runtime';
+//import Js functions used in app
+// import { handleSubmit } from './js/formHandler'
+// import { changeInputArea } from './js/changeInputType'
+// import { updateUI } from './js/updateUI'
+// import { checkURLInput } from './js/checkURLInput'
+// import { notifyError } from './js/notifyError'
+// import { clearUI } from './js/clearUI'
+// import { processInput } from './js/processInput'
+// import { postData } from './js/postData'
+
+//import saas styles
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/header.scss'
+import './styles/nav.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+
+// import meaning-cloud-logo from './meaning-cloud-logo.png';
+// import meaningCloudLogo from './views/img/meaning-cloud-logo.png'
+
+console.log("Entry point reloaded!");
+
+//export js files for use in app
+export {
+    // handleSubmit,
+    // changeInputArea,
+    // meaningCloudLogo,
+    // updateUI,
+    // checkURLInput,
+    // notifyError,
+    // clearUI,
+    // processInput,
+    // postData
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,2 +1,59 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// const Myfile-loader = require{'file-loader'}
+// const image-webpack-loader = require('image-webpack-loader')
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    output: {
+        crossOriginLoading: 'anonymous',
+        libraryTarget: 'var', 
+        library: 'Client',
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            //module loader images
+            {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'img/',
+            }
+        },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss', '.gif', '.png', '.jpg', 'jpeg', '.svg']
+    },
+    
+}
